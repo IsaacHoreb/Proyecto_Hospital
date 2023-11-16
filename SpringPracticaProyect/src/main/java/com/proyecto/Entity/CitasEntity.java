03f43adf2d62a77388fb217f2c60da9ba77d5201
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-11-16 09:32:31 -0600

    Atributos de Citas

--- SpringPracticaProyect/src/main/java/com/proyecto/Entity/CitasEntity.java
+++ SpringPracticaProyect/src/main/java/com/proyecto/Entity/CitasEntity.java
@@ -1,5 +1,44 @@
 package com.proyecto.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.proyecto.Entity.enums.EEstado;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "citas")
 public class CitasEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @NotBlank(message = "La fecha no puede estar vacio...")
+    @Column(name = "date", columnDefinition = "DATE", nullable = false)
+    private LocalDate date;
+
+    @NotBlank(message = "La hora no puede estar vacio...")
+    @Column(name = "hour", columnDefinition = "TIME", nullable = false)
+    private LocalTime hour;
+
+    @NotBlank(message = "El motivo no puede estar vacio...")
+    @Column(name = "motive", columnDefinition = "VARCHAR(50)", nullable = false)
+    private String motive;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "El estado no puede estar vacio...")
+    @Column(name = "stated", columnDefinition = "VARCHAR(10)", nullable = false)
+    private EEstado stated;
 
 }
