662b5f904fee9f052f6e38b61d72677aa39f7dc5
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-11-16 10:04:12 -0600

    Atributos de Sala

--- SpringPracticaProyect/src/main/java/com/proyecto/Entity/SalasEntity.java
+++ SpringPracticaProyect/src/main/java/com/proyecto/Entity/SalasEntity.java
@@ -1,5 +1,25 @@
 package com.proyecto.Entity;
 
+import com.proyecto.Entity.enums.ETipo;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "salas")
 public class SalasEntity {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "El tipo de sala no puede estar vacio...")
+    @Column(name = "tipo", columnDefinition = "VARCHAR(15)", nullable = false)
+    private ETipo tipo;
 }
