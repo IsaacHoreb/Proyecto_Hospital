989551e387a302b7586dcf23f10929f6e4ffc9a4
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-11-16 09:13:36 -0600

    Atributos de Historial medico

--- SpringPracticaProyect/src/main/java/com/proyecto/Entity/HistorialEntity.java
+++ SpringPracticaProyect/src/main/java/com/proyecto/Entity/HistorialEntity.java
@@ -1,5 +1,37 @@
 package com.proyecto.Entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "historial")
 public class HistorialEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "La fecha de creacion no puede estar vacio...")
+    @Column(name = "date_created", columnDefinition = "DATE", nullable = false)
+    private LocalDate dateCreated;
+
+    @NotBlank(message = "El diagnostico no puede estar vacio...")
+    @Column(name = "diagnoses", columnDefinition = "VARCHAR(25)", nullable = false)
+    private String diagnoses;
+
+    @NotBlank(message = "El tratamiento no puede estar vacio...")
+    @Column(name = "treatments", columnDefinition = "VARCHAR(25)", nullable = false)
+    private String treatments;
+
+    @Column(name = "medical_notes", columnDefinition = "VARCHAR(35)")
+    private String medicalNotes;
 
 }
