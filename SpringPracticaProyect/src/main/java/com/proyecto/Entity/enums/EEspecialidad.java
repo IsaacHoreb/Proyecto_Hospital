ca4d2d643600a9e1a8ba1e4700efe690ec59ad4a
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-11-16 02:19:38 -0600

    Creacion Enum especialidad de Medicos

---
+++ SpringPracticaProyect/src/main/java/com/proyecto/Entity/enums/EEspecialidad.java
@@ -0,0 +1,15 @@
+package com.proyecto.Entity.enums;
+
+public enum EEspecialidad {
+    CARDIOLOGIA,
+    DERMATOLOGIA,
+    ENDOCRINOLOGIA,
+    GASTROENTEROLOGIA,
+    GINECOLOGIA,
+    NEUROLOGIA,
+    OFTALMOLOGIA,
+    ORTOPEDIA,
+    PEDIATRIA,
+    UROLOGIA
+
+}
