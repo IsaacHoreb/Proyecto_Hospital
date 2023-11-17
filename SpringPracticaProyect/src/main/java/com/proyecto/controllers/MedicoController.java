4b7b75d7ce66bf7d8759b5db7b198b5c7cc373cf
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-11-16 23:47:04 -0600

    Creacion del Controllador Medico

---
+++ SpringPracticaProyect/src/main/java/com/proyecto/controllers/MedicoController.java
@@ -0,0 +1,38 @@
+package com.proyecto.controllers;
+
+import com.proyecto.Entity.MedicoEntity;
+import com.proyecto.services.MedicoServices;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/doctor")
+public class MedicoController {
+
+    @Autowired
+    private MedicoServices medicoServices;
+
+    @PostMapping("/save")
+    public ResponseEntity<MedicoEntity> saveDoctor(@Valid @RequestBody MedicoEntity medico) throws Exception {
+        return medicoServices.guardarMedico(medico);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<MedicoEntity> getDoctorId(@PathVariable("id") Long id) throws Exception {
+        return medicoServices.obtenerMedicoId(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<MedicoEntity> updateDoctorId(@PathVariable("id") Long id, @Valid @RequestBody MedicoEntity detalles) throws Exception {
+        return medicoServices.actualizarMedicoId(id, detalles);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteDoctorId(@PathVariable("id") Long id) throws Exception {
+        return medicoServices.eliminarMedicoId(id);
+    }
+
+
+}
