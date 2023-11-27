3b4468fd7ee2082bd887f822ccda0d9fb8cbb659
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-11-27 16:45:33 -0600

    Formato de datos para medicos

--- FrontedPractica/src/app/medicos.ts
+++ FrontedPractica/src/app/medicos.ts
@@ -1,2 +1,15 @@
+//Agrega el tipo de datos
 export class Medicos {
+
+    id: number;
+    name: string;
+    lastName: string;
+    age: number;
+    especialization: string;
+    phone: string;
+    email: string;
+    address: string;
+    startTime: string;
+    endTime: string;
+
 }
