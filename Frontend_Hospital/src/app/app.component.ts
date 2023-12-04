69201a3d2254d1ac8a3f67a62ce50a757d79b693
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-12-04 00:33:32 -0600

    Cambio de titulo

--- Frontend_Hospital/src/app/app.component.ts
+++ Frontend_Hospital/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Frontend_Hospital';
+  title = 'Hospital General De Estados Unidos Mexicanos';
 }
