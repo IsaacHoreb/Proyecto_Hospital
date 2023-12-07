ff29cffc6c51a9ba56f3b1ff4b3bcfe6a1bbdea1
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-12-07 00:01:57 -0600

    Agregados y cambios para eliminar un Paciente

--- Frontend_Hospital/src/app/listar-pacientes/listar-pacientes.component.ts
+++ Frontend_Hospital/src/app/listar-pacientes/listar-pacientes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pacientes } from '../pacientes';
 import { PacientesService } from '../pacientes.service';
 import { Router } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-listar-pacientes',
@@ -32,6 +33,34 @@ export class ListarPacientesComponent implements OnInit {
 
   detallesPaciente(id: number) {
     this.router.navigate(['pacienteDetalles', id]);
+  }
+
+  eliminarPaciente(id: number) {
+    swal({
+      title: '¿Estas seguro?',
+      text: "Confirma si deseas eliminar al Paciente",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si , elimínalo',
+      cancelButtonText: 'No, cancelar',
+      confirmButtonClass: 'btn btn-success',
+      cancelButtonClass: 'btn btn-danger',
+      buttonsStyling: true
+    }).then((result) => {
+      if (result.value) {
+        this.pacienteServices.eliminarPacientePoriD(id).subscribe(dato => {
+          console.log(dato);
+          this.obtenerPacienteLista();
+          swal(
+            'Paciente eliminado',
+            'El Paciente ha sido eliminado con exito',
+            'success'
+          )
+        })
+      }
+    })
   }
 
 }
