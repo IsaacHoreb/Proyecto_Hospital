594899547064945b7f97265a7e29dc05b935003c
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-12-09 22:38:44 -0600

    Agregados y cambios para el listado de Historial

--- Frontend_Hospital/src/app/historial.service.ts
+++ Frontend_Hospital/src/app/historial.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError } from 'rxjs';
+import { Historial } from './historial';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class HistorialService {
 
-  constructor() { }
+  //.-Ingresamos el endPoint del controlador de Historial
+  private baseURL = "http://localhost:8080/historial";
+
+  constructor(private httpClient: HttpClient) { }
+
+  obtenerListaDeHistorial(): Observable<Historial[]> {
+    return this.httpClient.get<Historial[]>(`${this.baseURL}/listAll`);
+  }
+
+  registarHitorial(historial: Historial): Observable<Object> {
+    return this.httpClient.post(`${this.baseURL}/save`, historial);
+  }
+
+  actualizarHistorial(id: number, historial: Historial): Observable<Object> {
+    return this.httpClient.put(`${this.baseURL}/update/${id}`, historial);
+  }
+
+  obtenerHistorialPorId(id: number): Observable<Historial> {
+    return this.httpClient.get<Historial>(`${this.baseURL}/get/${id}`);
+  }
+
+  eliminarHistorialPorId(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.baseURL}/delete/${id}`, { responseType: 'text' }).pipe(
+      catchError((error: any) => {
+        console.error('Error al eliminar el médico:', error);
+        throw error; // Puedes manejar el error de otra manera según tus necesidades
+      }));
+  }
+
 }
--- Frontend_Hospital/src/app/listar-historial/listar-historial.component.ts
+++ Frontend_Hospital/src/app/listar-historial/listar-historial.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Historial } from '../historial';
+import { HistorialService } from '../historial.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-historial',
   templateUrl: './listar-historial.component.html',
   styleUrls: ['./listar-historial.component.css']
 })
-export class ListarHistorialComponent {
+
+export class ListarHistorialComponent implements OnInit {
+
+  historial: Historial[]; //Objeto en Array
+
+  constructor(private historialSerices: HistorialService, private router: Router) { }
+
+  private obtenerHistorial() {
+    this.historialSerices.obtenerListaDeHistorial().subscribe(datos => {
+      this.historial = datos;
+    }), error => console.log(error);
+  }
+
+  ngOnInit(): void {
+    this.obtenerHistorial();
+
+  }
 
 }
