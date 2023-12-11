4b7e774bfad0add2bf0cb4aac9657bb516800813
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-12-10 21:42:21 -0600

    Agregados de los servicios para las citas

--- Frontend_Hospital/src/app/citas.service.ts
+++ Frontend_Hospital/src/app/citas.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Citas } from './citas';
+import { Observable, catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CitasService {
 
-  constructor() { }
+  //.-Ingresamos el endPoint del controlador de citas
+  private baseURL = "http://localhost:8080/citas";
+
+  constructor(private httpClient: HttpClient) { }
+
+  registarCitas(citas: Citas): Observable<Object> {
+    return this.httpClient.post(`${this.baseURL}/save`, citas);
+  }
+
+  obtenerListarDeCitas(): Observable<Citas[]> {
+    return this.httpClient.get<Citas[]>(`${this.baseURL}/listAll`);
+  }
+
+  actualizarCitasPorId(id: number, citas: Citas): Observable<Object> {
+    return this.httpClient.put(`${this.baseURL}/update/${id}`, citas);
+  }
+
+  obtenerCitasPorId(id: number): Observable<Citas> {
+    return this.httpClient.get<Citas>(`${this.baseURL}/get/${id}`);
+  }
+
+  eliminarCitasPorId(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.baseURL}/delete/${id}`, { responseType: 'text' }).pipe(
+      catchError((error: any) => {
+        console.error('Error al eliminar la cita:', error);
+        throw error; // Puedes manejar el error de otra manera según tus necesidades
+      }));
+  }
+
+
+
+
 }
