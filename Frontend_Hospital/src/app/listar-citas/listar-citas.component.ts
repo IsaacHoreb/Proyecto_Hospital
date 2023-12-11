1d37f9ce217a14db9f39e9975381cfc272bd36c7
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-12-10 22:26:40 -0600

    Agregados y cambios para el componente de listar citas

--- Frontend_Hospital/src/app/listar-citas/listar-citas.component.ts
+++ Frontend_Hospital/src/app/listar-citas/listar-citas.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Citas } from '../citas';
+import { CitasService } from '../citas.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-citas',
   templateUrl: './listar-citas.component.html',
   styleUrls: ['./listar-citas.component.css']
 })
-export class ListarCitasComponent {
+
+export class ListarCitasComponent implements OnInit {
+
+  citas: Citas[];
+
+  constructor(private citasServices: CitasService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.obtenerCitas();
+
+  }
+
+  private obtenerCitas() {
+    this.citasServices.obtenerListarDeCitas().subscribe(datos => {
+      this.citas = datos;
+    }), error => console.log(error);
+  }
+
 
 }
