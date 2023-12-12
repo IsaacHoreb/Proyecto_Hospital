c2cd7307bf016db7292fdbcb1512ea4dbdcb271c
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-12-12 01:24:54 -0600

    Cambios y agregados para los servicios del departamento

--- Frontend_Hospital/src/app/departamentos.service.ts
+++ Frontend_Hospital/src/app/departamentos.service.ts
@@ -1,9 +1,49 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Departamentos } from './departamentos';
+import { Observable, catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartamentosService {
 
-  constructor() { }
+  //.-Ingresamos el endPoint del controlador de departamentos
+  private baseURL = "http://localhost:8080/departamentos";
+
+  constructor(private httpClient: HttpClient) { }
+
+  registarDepartamentos(depart: Departamentos): Observable<Object> {
+    return this.httpClient.post(`${this.baseURL}/save`, depart);
+  }
+
+  obtenerListaDeDepartamentos(): Observable<Departamentos[]> {
+    return this.httpClient.get<Departamentos[]>(`${this.baseURL}/listAll`);
+  }
+
+  actualizarDepartamentosPorId(id: number, depart: Departamentos): Observable<Object> {
+    return this.httpClient.put(`${this.baseURL}/update/${id}`, depart);
+  }
+
+  obtenerDepartamentoPorId(id: number): Observable<Departamentos> {
+    return this.httpClient.get<Departamentos>(`${this.baseURL}/get/${id}`);
+  }
+
+  eliminarDepartamentoPorId(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.baseURL}/delete/${id}`, { responseType: 'text' }).pipe(
+      catchError((error: any) => {
+        console.error('Error al eliminar el departamento:', error);
+        throw error; // Puedes manejar el error de otra manera según tus necesidades
+      }));
+  }
+
+
+
+
+
+
+
+
+
+
 }
