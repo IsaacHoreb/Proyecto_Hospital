9c0f2e087d6f305005decab1bbe4c8e4ef67a827
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-12-12 12:52:26 -0600

    Cambios y agregados para el listado de los Departamentos

--- Frontend_Hospital/src/app/listar-departamentos/listar-departamentos.component.ts
+++ Frontend_Hospital/src/app/listar-departamentos/listar-departamentos.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DepartamentosService } from '../departamentos.service';
+import { Router } from '@angular/router';
+import { Departamentos } from '../departamentos';
 
 @Component({
   selector: 'app-listar-departamentos',
   templateUrl: './listar-departamentos.component.html',
   styleUrls: ['./listar-departamentos.component.css']
 })
-export class ListarDepartamentosComponent {
+
+export class ListarDepartamentosComponent implements OnInit {
+
+  departamento: Departamentos[];
+
+  constructor(private departaServices: DepartamentosService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.obtenerDepart();
+  }
+
+  private obtenerDepart(){
+    this.departaServices.obtenerListaDeDepartamentos().subscribe(d => {
+      this.departamento = d;
+    }),e => console.log(e);
+  }
+
 
 }
