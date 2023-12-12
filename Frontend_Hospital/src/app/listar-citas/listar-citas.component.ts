75a8e0832f4576de58c7c19de184bfcb7d7e7c6d
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-12-12 00:41:19 -0600

    Cambios y agregados para eliminar una cita

--- Frontend_Hospital/src/app/listar-citas/listar-citas.component.ts
+++ Frontend_Hospital/src/app/listar-citas/listar-citas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Citas } from '../citas';
 import { CitasService } from '../citas.service';
 import { Router } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-listar-citas',
@@ -32,6 +33,34 @@ export class ListarCitasComponent implements OnInit {
 
   verDetallesCitas(id: number) {
     this.router.navigate(['/citasDetalles', id]);
+  }
+
+  eliminarHistorial(id: number) {
+    swal({
+      title: '¿Estas seguro?',
+      text: "Confirma si deseas eliminar la cita",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si , elimínalo',
+      cancelButtonText: 'No, cancelar',
+      confirmButtonClass: 'btn btn-success',
+      cancelButtonClass: 'btn btn-danger',
+      buttonsStyling: true
+    }).then((result) => {
+      if (result.value) {
+        this.citasServices.eliminarCitasPorId(id).subscribe(dato => {
+          console.log(dato);
+          this.obtenerCitas();
+          swal(
+            'Cita eliminada',
+            'La cita ha sido eliminado con exito',
+            'success'
+          )
+        })
+      }
+    })
   }
 
 }
