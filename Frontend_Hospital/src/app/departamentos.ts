eaa76c8432ed429f09b34fb8d819b6aa421fd461
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-12-12 01:12:28 -0600

    Cambios y agregado de tipo de datos para departamentos

--- Frontend_Hospital/src/app/departamentos.ts
+++ Frontend_Hospital/src/app/departamentos.ts
@@ -1,2 +1,11 @@
 export class Departamentos {
+
+    id: number;
+    nameDepartment: string[];
+    description: string;
+
+    setNameDepartamentos(nameDepartento: string) {
+        this.nameDepartment = [nameDepartento];
+    }
+
 }
