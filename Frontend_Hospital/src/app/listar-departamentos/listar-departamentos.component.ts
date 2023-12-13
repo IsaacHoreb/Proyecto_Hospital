580ab6748d7dbdae2202bbe1902f50f04d89b066
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-12-13 01:26:36 -0600

    Cambios y agregados para eliminar un Depart

--- Frontend_Hospital/src/app/listar-departamentos/listar-departamentos.component.ts
+++ Frontend_Hospital/src/app/listar-departamentos/listar-departamentos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DepartamentosService } from '../departamentos.service';
 import { Router } from '@angular/router';
 import { Departamentos } from '../departamentos';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-listar-departamentos',
@@ -32,5 +33,34 @@ export class ListarDepartamentosComponent implements OnInit {
   verDetallesDepartamentos(id: number) {
     this.router.navigate(['/departamentoDetalles', id]);
   }
+
+  eliminarDepartamento(id: number) {
+    swal({
+      title: '¿Estas seguro?',
+      text: "Confirma si deseas eliminar el departamento",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si , elimínalo',
+      cancelButtonText: 'No, cancelar',
+      confirmButtonClass: 'btn btn-success',
+      cancelButtonClass: 'btn btn-danger',
+      buttonsStyling: true
+    }).then((result) => {
+      if (result.value) {
+        this.departaServices.eliminarDepartamentoPorId(id).subscribe(dato => {
+          console.log(dato);
+          this.obtenerDepart();
+          swal(
+            'Departamento eliminado',
+            'El departamento ha sido eliminado con exito',
+            'success'
+          )
+        })
+      }
+    })
+  }
+
 
 }
