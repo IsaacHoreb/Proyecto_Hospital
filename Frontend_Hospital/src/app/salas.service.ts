457e2197a19204c1ce87fcd9750869d111a158ce
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-12-15 00:32:09 -0600

    Agregados de los servicios para la Sala

--- Frontend_Hospital/src/app/salas.service.ts
+++ Frontend_Hospital/src/app/salas.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError } from 'rxjs';
+import { Salas } from './salas';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SalasService {
 
-  constructor() { }
+  //.-Ingresamos el endPoint del controlador de Salas
+  private baseURL = "http://localhost:8080/salas";
+
+  constructor(private httpClient: HttpClient) { }
+
+  obtenerListaDeSalas(): Observable<Salas[]> {
+    return this.httpClient.get<Salas[]>(`${this.baseURL}/listAll`);
+  }
+
+  registarSala(sala: Salas): Observable<Object> {
+    return this.httpClient.post(`${this.baseURL}/save`, sala);
+  }
+
+  actualizarSala(id: number, sala: Salas): Observable<Object> {
+    return this.httpClient.put(`${this.baseURL}/update/${id}`, sala);
+  }
+
+  obtenerSalaPorId(id: number): Observable<Salas> {
+    return this.httpClient.get<Salas>(`${this.baseURL}/get/${id}`);
+  }
+
+  eliminarSalaPorId(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.baseURL}/delete/${id}`, { responseType: 'text' }).pipe(
+      catchError((error: any) => {
+        console.error('Error al eliminar la sala', error)
+        throw error; // Puedes manejar el error de otra manera según tus necesidades
+      }));
+  }
+
 }
