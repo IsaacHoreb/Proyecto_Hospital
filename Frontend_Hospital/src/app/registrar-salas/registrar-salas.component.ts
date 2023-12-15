ee19f9abd2e3c21bddc3604e11d06ceac013cffd
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-12-15 01:59:05 -0600

    Cambios y agregados para el componente de registrar salas

--- Frontend_Hospital/src/app/registrar-salas/registrar-salas.component.ts
+++ Frontend_Hospital/src/app/registrar-salas/registrar-salas.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Salas } from '../salas';
+import { SalasService } from '../salas.service';
+import { Router } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registrar-salas',
   templateUrl: './registrar-salas.component.html',
   styleUrls: ['./registrar-salas.component.css']
 })
-export class RegistrarSalasComponent {
+
+export class RegistrarSalasComponent implements OnInit {
+
+  sala: Salas = new Salas();
+
+  constructor(private salaServices: SalasService, private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit() {
+    this.guardarMedico();
+    //console.log(this.sala);
+  }
+
+  guardarMedico() {
+    this.salaServices.registarSala(this.sala).subscribe(dato => {
+      this.irListaDeSalas();
+    }), error => console.log(error);
+  }
+
+  irListaDeSalas() {
+    this.router.navigate(['/salas']);
+    swal('Sala Registrado', `El medico ${this.sala.type} ha sido registrado con éxito`, 'success');
+  }
+
+  tipoSala: string[] = ['CONSULTA', 'QUIROFANO', 'SALA_DE_ESPERA'];
 
 }
