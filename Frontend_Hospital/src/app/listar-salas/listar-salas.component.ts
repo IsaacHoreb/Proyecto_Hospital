880aa88c595df67018772a2a78fa19cd3830787e
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-12-15 02:47:36 -0600

    Agregados para eliminar una sala

--- Frontend_Hospital/src/app/listar-salas/listar-salas.component.ts
+++ Frontend_Hospital/src/app/listar-salas/listar-salas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SalasService } from '../salas.service';
 import { Router } from '@angular/router';
 import { Salas } from '../salas';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-listar-salas',
@@ -31,6 +32,34 @@ export class ListarSalasComponent implements OnInit {
 
   detallesSalasID(id: number) {
     this.router.navigate(['/salaDetalles', id]);
+  }
+
+  eliminarSalaPorId(id: number) {
+    swal({
+      title: '¿Estas seguro?',
+      text: "Confirma si deseas eliminar la sala",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si , elimínalo',
+      cancelButtonText: 'No, cancelar',
+      confirmButtonClass: 'btn btn-success',
+      cancelButtonClass: 'btn btn-danger',
+      buttonsStyling: true
+    }).then((result) => {
+      if (result.value) {
+        this.salaServices.eliminarSalaPorId(id).subscribe(dato => {
+          //console.log(dato);
+          this.obtenerSalaLista();
+          swal(
+            'Sala eliminado',
+            'La sala ha sido eliminado con exito',
+            'success'
+          )
+        })
+      }
+    })
   }
 
 }
